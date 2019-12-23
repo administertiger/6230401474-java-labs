4321c20608fdf5e53a4b00bffa668b1b4aa1aa6b
TIIGER♥ <58902019+administertiger@users.noreply.github.com>
2019-12-23 15:58:30 +0700

    Add files via upload

--- LAB3/pongsuwan/surapat/lab3/NumberGuessingMethodGame.java
+++ LAB3/pongsuwan/surapat/lab3/NumberGuessingMethodGame.java
@@ -1,59 +1,64 @@
-/*
-Author: Surapat Pongsuwan
-ID: 623040147-4
-Sec: 2
-Date: December 23, 2019
-*/
+//package pongsuwan.surapat.lab3;
 
 import java.util.Scanner;
-import java.util.Random;
 
-public class NumberGuessingGame {
-    public static void main(String[] args) {
 
-        System.out.println("Welcome to a number guessing game!");
+public class NumberGuessingMethodGame {
+    static int answer;
 
+    public static void main(String[] args) 
+    {
+        genAnswer();
+        playGame();
+    }
+
+    static void genAnswer() 
+    {
+        int min = 1;
+        int max = 10;
 
         //random number
-        Random number = new Random();
-        int randomNumber = number.nextInt(10)+1; //receiever
+        answer = min + (int)(Math.random()*(max-min)+1);
+    }
 
+    static void playGame() 
+    {
+        System.out.println("Welcome to a number guessing game!");
 
-        //Scanner of number
+        //Scanner of guess
         Scanner input = new Scanner(System.in);
 
-
         int count = 0;
-
 
         //get in loop
         for (int i=0; i<5; i++) {
             System.out.print("Enter an integer between 1 and 10:");
+            
             int guess = input.nextInt();
             count++;
 
-            if (guess == randomNumber) {
+            if (guess == answer) {
                 System.out.println("Congretulation!");
                 System.out.println("You have tried " + count + " times.");
                 System.exit(0);
             }
-            else if (guess > randomNumber) {
+            else if (guess > answer) {
                 System.out.println("Try a lower number!");
             }
-            else if (guess < randomNumber) {
+            else if (guess < answer) {
                 System.out.println("Try a higher number!");
             }
+            
         }
-
 
         if (count == 5) {
             System.out.println("You have tried " + count + " times. You ran out of guesses");
         }
-
         else {
             System.out.println("You have tried " + count + " times");
         }
         
-        System.out.println("The Answer is " + randomNumber);
+        System.out.println("The Answer is " + answer);
     }
+    
 }
