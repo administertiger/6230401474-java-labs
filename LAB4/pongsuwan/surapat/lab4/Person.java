845423762a384f555faaee7ca812b44a4d770537
TIIGER♥ <58902019+administertiger@users.noreply.github.com>
2020-01-06 16:05:31 +0700

    Add files via upload

---
+++ LAB4/pongsuwan/surapat/lab4/Person.java
@@ -0,0 +1,74 @@
+import java.text.DecimalFormat;
+
+public class Person {
+    private String name;
+    private Double height;
+    private Double weight;
+    private static int numberOfPerson = 0;
+
+    public Person()
+    {
+        this.name = "unknown";
+        this.height = 0.0;
+        this.weight = 0.0;
+        numberOfPerson += 1;
+    }
+
+    public Person(String namePerson)
+    {
+        this.name = namePerson;
+        this.height = 0.0;
+        this.weight = 0.0;
+        numberOfPerson += 1;
+    }
+
+    public Person(String namePerson, double heightPerson, double weightPerson)
+    {
+        this.name = namePerson;
+        this.height = heightPerson;
+        this.weight = weightPerson;
+        numberOfPerson += 1;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public Double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(Double newHeight) {
+        this.height = newHeight;
+    }
+
+    public Double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(Double newWeight) {
+        this.weight = newWeight;
+    }
+
+    DecimalFormat df = new DecimalFormat("#0.00");
+
+    public String getBMI() {
+        String BMI = df.format(this.weight / ((this.height / 100) * (this.height / 100)));
+        return BMI;
+    }
+
+    public static int getNumberOfPerson() {
+        return numberOfPerson;
+    }
+    @Override
+    public String toString()
+    {
+        return "name=" + name + ", height=" + height + " cm. weight=" + weight + " kg." ;
+    }
+
+}
+
