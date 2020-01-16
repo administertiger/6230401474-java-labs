7bc8ae74ab6bda59fba5a7098d6a5005904d55ec
TIIGER♥ <58902019+administertiger@users.noreply.github.com>
2020-01-16 17:41:12 +0700

    update lab

---
+++ LAB5/pongsuwan/surapat/lab5/Moveable.java
@@ -0,0 +1,20 @@
+package pongsuwan.surapat.lab5;
+
+/*
+* This program is to create an interface class "Moveable" which contain abstract medthods.
+*
+* Author : Surapat Pongsuwan
+* ID : 623040147-4
+* section : 2
+* Date : January 16, 2020
+*/
+
+
+interface Moveable {
+
+    // abstact medthods.
+    public void moveUp(int val);
+    public void moveDown(int val);
+    public void moveRight(int val);
+    public void moveLeft(int val);
+}
